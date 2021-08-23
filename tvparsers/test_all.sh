5883f59703134c3774671656f5bbe70ad51f23ef
zikwall <andrey.kapitonov.96@gmail.com>
2021-08-23 11:02:31 +0300

    add shell script for execute all files

---
+++ tvparsers/test_all.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir -p $(pwd)/tmp/all
+
+for filename in $(pwd)/*.py; do
+  file=$(basename $filename)
+
+  echo "Execute $file output to ./tmp/all/$file.csv"
+
+  python3 $file --output $(pwd)/tmp/all
+
+  #sql=$(<$filename)
+  #docker run --rm --network="clickhouse-net" --link clickhouse-$1:clickhouse-server yandex/clickhouse-client -m --host clickhouse-server --query="$sql"
+done
