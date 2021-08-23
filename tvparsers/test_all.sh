a3e202b494fe258fbdffa5dd6fea702e19f3649f
zikwall <andrey.kapitonov.96@gmail.com>
2021-08-23 11:24:26 +0300

    fix: output filename

--- tvparsers/test_all.sh
+++ tvparsers/test_all.sh
@@ -7,7 +7,7 @@ for filename in $(pwd)/*.py; do
 
   echo "Execute $file output to ./tmp/all/$file.csv"
 
-  python3 $file --output $(pwd)/tmp/all
+  python3 $file --output $(pwd)/tmp/all/$file.csv
 
   #sql=$(<$filename)
   #docker run --rm --network="clickhouse-net" --link clickhouse-$1:clickhouse-server yandex/clickhouse-client -m --host clickhouse-server --query="$sql"
